da35cc8098f543bd0ad89a6108170b85beadcacc
mibshara <bsharapc@gmail.com>
2017-11-28 15:37:14 -0600

    added files after fixing epic failure

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:index, :shop]
+  
+  def index
+    @products = Product.all
+  end
+  def shop
+    @products = Product.all
+  end
+end
---
+++ app/models/product.rb
@@ -0,0 +1,25 @@
+class Product < ActiveRecord::Base
+
+  before_destroy :ensure_not_product_item
+
+  has_many :product_items
+
+  belongs_to :category
+
+  validates :title, :description, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  def ensure_not_product_item
+    if product_itmes.empty?
+      return true
+    else
+      errors.add(:base, 'You have Product Items')
+      return false
+    end
+  end
+
+end
