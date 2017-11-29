17a3bc0cfcb6423a11d9c2a9e693c58ace4f8ff8
mibshara <bsharapc@gmail.com>
2017-11-28 22:43:19 -0600

    added search by keyword

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,17 @@
 class PagesController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:index, :shop]
-  
+
   def index
     @products = Product.all
   end
+
   def shop
-    @products = Product.all
-  end
+      if params[:search]
+      @products = Product.search(params[:search])
+      else
+        @products = Product.all
+
+      end
+end
 end
