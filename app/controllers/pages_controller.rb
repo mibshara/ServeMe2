cb56e876b84799ef9c6a0e404968a4c42e7e93f1
mibshara <bsharapc@gmail.com>
2017-11-29 01:41:06 -0600

    fixed a cart problem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,12 @@
 class PagesController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:index, :shop, :show]
+  before_action :set_cart, only: [:index,:show, :shop]
 
   def index
     @products = Product.all
   end
 
   def show
-
     @product = Product.find(params[:number].to_i)
   end
 
@@ -16,7 +15,6 @@ class PagesController < ApplicationController
       @products = Product.search(params[:search])
       else
       @products = Product.all
-
       end
-end
+    end
 end
